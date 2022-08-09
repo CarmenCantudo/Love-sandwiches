2be8cb206c9a6bf1697b4633fa0c55b9389e97b5
Carmen <carmencantudo@gmail.com>
2022-08-09 23:28:10 +0000

    Create geunction to get the stock values to know the number of sandwiches necesary for next market

--- run.py
+++ run.py
@@ -129,7 +129,22 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+stock_data = main()
+
+def get_stock_values(data):
+    """
+    Get the headings and values to create a dictionary and print it with
+    the stock necesary for next market
+    """
+    headings = SHEET.worksheet('stock').row_values(1)
+    dictionary = dict(zip(headings,data))
+    
+    return dictionary
+
+print("Make the following number of sandwiches for next market:\n")
+stock_values = get_stock_values(stock_data)
+print(stock_values)
